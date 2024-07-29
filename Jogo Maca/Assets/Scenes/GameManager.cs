ed098364c47464b270f81be48aa5542a2d92bf54
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-07-29 10:41:45 -0300

    metodo LoseLife

--- Jogo Maca/Assets/Scenes/GameManager.cs
+++ Jogo Maca/Assets/Scenes/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
-    
+
     private Vector2 screenBounds;
     private int score;
     private int lifes = 3;
@@ -14,10 +14,26 @@ public class GameManager : MonoBehaviour
     public Vector2 ScreenBounds { get => screenBounds; }
     public int Lifes { get => lifes; }
 
-    void Awake()
+    public void Awake()
     {
         instance = this;
         screenBounds = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)) + new Vector3(-1, 1);
     }
 
+    public void LoseLife()
+    {
+        lifes -= 1;
+        ManagerUI.instance.UpdateLifeText();
+
+        if (lifes == 0)
+        {
+            if (score > PlayerPrefs.GetInt("Record", 0))
+            {
+                PlayerPrefs.SetInt("Record", score);
+            }
+            Time.timeScale = 0;
+            ManagerUI.instance.GameOver();
+        }
+
+    }
 }
