05d6485d1e7d741955c390185ea18057e12cffba
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-07-29 10:25:53 -0300

    Encapsulei as variáveis

--- Jogo Maca/Assets/Scenes/GameManager.cs
+++ Jogo Maca/Assets/Scenes/GameManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
+    private static GameManager instance;
 
     private Vector2 screenBounds;
     private int score;
     private int lifes = 3;
 
+    public static GameManager Instance { get => instance;}
+    public Vector2 ScreenBounds { get => screenBounds;}
+    public int Score { get => score;}
+    public int Lifes { get => lifes;}
 }
