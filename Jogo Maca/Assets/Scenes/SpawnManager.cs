73c2f7ebfdcd4c9ac3662b24491a998bfc60b38c
ipysk <sarahjuliaantunesv@gmail.com>
2024-07-29 10:48:49 -0300

    Spawn Manager

--- Jogo Maca/Assets/Scenes/SpawnManager.cs
+++ Jogo Maca/Assets/Scenes/SpawnManager.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] GameObject redApple, greenApple, goldenApple;
+    float timer;
+    float initialTime = 1.2f;
+    int appleType;
     // Start is called before the first frame update
-    void Start()
+    void Spawn()
     {
-        
+        if (timer <= 0)
+        {
+            int appleType = Range.Random(0, 101);
+        }
+        if (appleType > 90)
+        {
+            Instantiate(goldenApple, new Vector3(Random.Range(-GameManager.instance.ScreenBounds.x, GameManager.instance.ScreenBounds.x), GameManager.instance.ScreenBounds.y, 0), Quaternion.identity);
+        }
+        else if (appleType > 60)
+        {
+            Instantiate(greenApple, new Vector3(Random.Range(-GameManager.instance.ScreenBounds.x, GameManager.instance.ScreenBounds.x), GameManager.instance.ScreenBounds.y, 0), Quaternion.identity);
+        }
+        else if (appleType <60)
+        {
+            Instantiate(goldenApple, new Vector3(Random.Range(-GameManager.instance.ScreenBounds.x, GameManager.instance.ScreenBounds.x), GameManager.instance.ScreenBounds.y, 0), Quaternion.identity);
+        }
+        if (initialTime > 0.8)
+        {
+            initialTime -= 0.5f;
+        }
+        timer = initialTime;
+        Time.deltaTime -= timer;
     }
 
     // Update is called once per frame
