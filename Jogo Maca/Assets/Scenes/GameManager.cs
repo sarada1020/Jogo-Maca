0d8af5658c537700bd9970a0f5c0842409d920a6
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-07-29 10:23:06 -0300

    Criação de variáveis

--- Jogo Maca/Assets/Scenes/GameManager.cs
+++ Jogo Maca/Assets/Scenes/GameManager.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Vector2 screenBounds;
+    private int score;
+    private int lifes = 3;
+
 }
