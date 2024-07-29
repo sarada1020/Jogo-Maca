973df23a97e5cdf2ce063804a1aafecd277d47bc
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-07-29 10:32:52 -0300

    Metodo Awake

--- Jogo Maca/Assets/Scenes/GameManager.cs
+++ Jogo Maca/Assets/Scenes/GameManager.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
-
+    
     private Vector2 screenBounds;
     private int score;
     private int lifes = 3;
 
-    public static GameManager Instance { get => instance;}
-    public Vector2 ScreenBounds { get => screenBounds;}
-    public int Lifes { get => lifes;}
+    public static GameManager Instance { get => instance; }
+    public Vector2 ScreenBounds { get => screenBounds; }
+    public int Lifes { get => lifes; }
+
+    void Awake()
+    {
+        instance = this;
+        screenBounds = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)) + new Vector3(-1, 1);
+    }
+
 }
