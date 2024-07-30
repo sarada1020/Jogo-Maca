aa42f49da2f0608fcbdcbe27f80fff66b698bb82
ipysk <sarahjuliaantunesv@gmail.com>
2024-07-30 14:18:39 -0300

    arrumação do spawn manager

--- Jogo Maca/Assets/Scenes/GameManager.cs
+++ Jogo Maca/Assets/Scenes/GameManager.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
+    private static GameManager Instance;
 
     private Vector2 screenBounds;
     private int score;
     private int lifes = 3;
 
-    public static GameManager Instance { get => instance; }
+    public static GameManager Instance { get => Instance; }
     public Vector2 ScreenBounds { get => screenBounds; }
     public int Lifes { get => lifes; }
 
     void Awake()
     {
-        instance = this;
+        Instance = this;
         screenBounds = New Vector3 (point.position.x = -1, point.position.y = 1 + mainCamera.ScreenToWorldPoint(new Vector3 point.position.x = Screen.width, point.position.y = Screen.heigth));
     }
     public void LoseLife()
--- Jogo Maca/Assets/Scenes/SpawnManager.cs
+++ Jogo Maca/Assets/Scenes/SpawnManager.cs
@@ -8,36 +8,30 @@ public class SpawnManager : MonoBehaviour
     float timer;
     float initialTime = 1.2f;
     int appleType;
-    // Start is called before the first frame update
     void Spawn()
     {
         if (timer <= 0)
         {
-            int appleType = Range.Random(0, 101);
-        }
-        if (appleType > 90)
-        {
-            Instantiate(goldenApple, new Vector3(Random.Range(-GameManager.instance.ScreenBounds.x, GameManager.instance.ScreenBounds.x), GameManager.instance.ScreenBounds.y, 0), Quaternion.identity);
-        }
-        else if (appleType > 60)
-        {
-            Instantiate(greenApple, new Vector3(Random.Range(-GameManager.instance.ScreenBounds.x, GameManager.instance.ScreenBounds.x), GameManager.instance.ScreenBounds.y, 0), Quaternion.identity);
-        }
-        else if (appleType <60)
-        {
-            Instantiate(goldenApple, new Vector3(Random.Range(-GameManager.instance.ScreenBounds.x, GameManager.instance.ScreenBounds.x), GameManager.instance.ScreenBounds.y, 0), Quaternion.identity);
-        }
-        if (initialTime > 0.8)
-        {
-            initialTime -= 0.05f;
-        }
-        timer = initialTime;
-        else { timer -= Time.deltaTime; }
-    }
+            int appleType = Random.Range(0, 101);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (appleType > 90)
+            {
+                Instantiate(goldenApple, new Vector3(Random.Range(-GameManager.Instance.ScreenBounds.x, GameManager.Instance.ScreenBounds.x), GameManager.Instance.ScreenBounds.y, 0), Quaternion.identity);
+            }
+            else if (appleType > 60)
+            {
+                Instantiate(greenApple, new Vector3(Random.Range(-GameManager.Instance.ScreenBounds.x, GameManager.Instance.ScreenBounds.x), GameManager.Instance.ScreenBounds.y, 0), Quaternion.identity);
+            }
+            else if (appleType < 60)
+            {
+                Instantiate(goldenApple, new Vector3(Random.Range(-GameManager.Instance.ScreenBounds.x, GameManager.Instance.ScreenBounds.x), GameManager.Instance.ScreenBounds.y, 0), Quaternion.identity);
+            }
+            if (initialTime > 0.8)
+            {
+                initialTime -= 0.05f;
+            }
+            timer = initialTime;
+        }
+        else { timer -= Time.deltaTime; }
     }
 }
