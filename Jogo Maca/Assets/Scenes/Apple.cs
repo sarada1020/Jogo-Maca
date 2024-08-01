9c005995f9061de7f0a2754f0ab4e9c9ea053f97
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-08-01 14:03:07 -0300

    Metodo OnTriggerEnter2D

--- Jogo Maca/Assets/Scenes/Apple.cs
+++ Jogo Maca/Assets/Scenes/Apple.cs
@@ -22,4 +22,13 @@ public class Apple : MonoBehaviour
     }
 
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"));
+        {
+
+        }
+
+    }
+
 }
